93789915a3d9d7aaf665a4a725edb7b9a78f44d5
msvstj <savastijonok@gmail.com>
2024-04-17 15:26:14 +0300

    chore: update sdk router

--- apps/web/src/graphql/data/apollo/client.ts
+++ apps/web/src/graphql/data/apollo/client.ts
@@ -1,13 +1,13 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { Reference, relayStylePagination } from '@apollo/client/utilities'
-import { createSubscriptionLink } from 'utilities/src/apollo/SubscriptionLink'
-import { splitSubscription } from 'utilities/src/apollo/splitSubscription'
+// import { createSubscriptionLink } from 'utilities/src/apollo/SubscriptionLink'
+// import { splitSubscription } from 'utilities/src/apollo/splitSubscription'
 
 const API_URL = process.env.REACT_APP_AWS_API_ENDPOINT
 const REALTIME_URL = process.env.REACT_APP_AWS_REALTIME_ENDPOINT
 const REALTIME_TOKEN = process.env.REACT_APP_AWS_REALTIME_TOKEN
 if (!API_URL || !REALTIME_URL || !REALTIME_TOKEN) {
-  throw new Error('AWS CONFIG MISSING FROM ENVIRONMENT')
+  // throw new Error('AWS CONFIG MISSING FROM ENVIRONMENT')
 }
 
 const httpLink = new HttpLink({ uri: API_URL })
@@ -17,7 +17,7 @@ export const apolloClient = new ApolloClient({
   link: httpLink,
   headers: {
     'Content-Type': 'application/json',
-    Origin: 'https://app.uniswap.org',
+    Origin: '',
   },
   cache: new InMemoryCache({
     typePolicies: {
@@ -83,5 +83,5 @@ export const apolloClient = new ApolloClient({
 })
 
 // This is done after creating the client so that client may be passed to `createSubscriptionLink`.
-const subscriptionLink = createSubscriptionLink({ uri: REALTIME_URL, token: REALTIME_TOKEN }, apolloClient)
-apolloClient.setLink(splitSubscription(subscriptionLink, httpLink))
+// const subscriptionLink = createSubscriptionLink({ uri: REALTIME_URL, token: REALTIME_TOKEN }, apolloClient)
+// apolloClient.setLink(splitSubscription(subscriptionLink, httpLink))
