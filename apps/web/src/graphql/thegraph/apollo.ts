1bc7c4a9eb844fe04a44689f46b36f28c2310c63
msvstj <savastijonok@gmail.com>
2024-04-19 12:17:22 +0300

    feat: patch smart-order and universal-router libs, add temp subgraph URL

--- apps/web/src/graphql/thegraph/apollo.ts
+++ apps/web/src/graphql/thegraph/apollo.ts
@@ -14,6 +14,7 @@ const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [ChainId.BASE]: 'https://api.studio.thegraph.com/query/48211/uniswap-v3-base/version/latest?source=uniswap',
   [ChainId.BLAST]:
     'https://gateway-arbitrum.network.thegraph.com/api/0ae45f0bf40ae2e73119b44ccd755967/subgraphs/id/2LHovKznvo8YmKC9ZprPjsYAZDCc4K5q4AYz8s3cnQn1',
+  [ChainId.REDSTONE_GARNET]: 'http://stg.swap-redstone.w3us.site:8000/subgraphs/name/swap-v3-redstone-garnet',
 }
 
 const httpLink = new HttpLink({ uri: CHAIN_SUBGRAPH_URL[ChainId.MAINNET] })
@@ -64,5 +65,9 @@ export const chainToApolloClient: Record<number, ApolloClient<NormalizedCacheObj
   [ChainId.AVALANCHE]: new ApolloClient({
     cache: new InMemoryCache(),
     uri: CHAIN_SUBGRAPH_URL[ChainId.AVALANCHE],
+  }),
+  [ChainId.REDSTONE_GARNET]: new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: CHAIN_SUBGRAPH_URL[ChainId.REDSTONE_GARNET],
   }),
 }
