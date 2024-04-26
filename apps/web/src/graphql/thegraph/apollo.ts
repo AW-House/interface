39291b48e2bb7c0f0f5b238631e2b78d1f30ef47
msvstj <savastijonok@gmail.com>
2024-04-26 17:09:54 +0300

    chore: update links

--- apps/web/src/graphql/thegraph/apollo.ts
+++ apps/web/src/graphql/thegraph/apollo.ts
@@ -14,8 +14,8 @@ const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [ChainId.BASE]: 'https://api.studio.thegraph.com/query/48211/uniswap-v3-base/version/latest?source=uniswap',
   [ChainId.BLAST]:
     'https://gateway-arbitrum.network.thegraph.com/api/0ae45f0bf40ae2e73119b44ccd755967/subgraphs/id/2LHovKznvo8YmKC9ZprPjsYAZDCc4K5q4AYz8s3cnQn1',
-  [ChainId.REDSTONE_GARNET]: 'http://stg.swap-redstone.w3us.site:8000/subgraphs/name/swap-v3-redstone-garnet',
-  [ChainId.REDSTONE]: 'http://stg.swap-redstone.w3us.site:8000/subgraphs/name/swap-v3-redstone-mainnet',
+  [ChainId.REDSTONE_GARNET]: 'https://graph.stg.swap-redstone.w3us.site/subgraphs/name/swap-v3-redstone-garnet',
+  [ChainId.REDSTONE]: 'https://graph.stg.swap-redstone.w3us.site/subgraphs/name/swap-v3-redstone-mainnet',
 }
 
 const httpLink = new HttpLink({ uri: CHAIN_SUBGRAPH_URL[ChainId.MAINNET] })
