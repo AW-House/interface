390dc8707d829d0f50e04728e72b8a7f825761fd
msvstj <savastijonok@gmail.com>
2024-04-30 14:40:56 +0300

    chore: temp remove graph links

--- apps/web/src/graphql/thegraph/apollo.ts
+++ apps/web/src/graphql/thegraph/apollo.ts
@@ -14,8 +14,8 @@ const CHAIN_SUBGRAPH_URL: Record<number, string> = {
   [ChainId.BASE]: 'https://api.studio.thegraph.com/query/48211/uniswap-v3-base/version/latest?source=uniswap',
   [ChainId.BLAST]:
     'https://gateway-arbitrum.network.thegraph.com/api/0ae45f0bf40ae2e73119b44ccd755967/subgraphs/id/2LHovKznvo8YmKC9ZprPjsYAZDCc4K5q4AYz8s3cnQn1',
-  [ChainId.REDSTONE_GARNET]: 'https://graph.stg.swap-redstone.w3us.site/subgraphs/name/swap-v3-redstone-garnet',
-  [ChainId.REDSTONE]: 'https://graph.stg.swap-redstone.w3us.site/subgraphs/name/swap-v3-redstone-mainnet',
+  // [ChainId.REDSTONE_GARNET]: 'https://graph.stg.swap-redstone.w3us.site/subgraphs/name/swap-v3-redstone-garnet',
+  // [ChainId.REDSTONE]: 'https://graph.stg.swap-redstone.w3us.site/subgraphs/name/swap-v3-redstone-mainnet',
 }
 
 const httpLink = new HttpLink({ uri: CHAIN_SUBGRAPH_URL[ChainId.MAINNET] })
@@ -67,12 +67,12 @@ export const chainToApolloClient: Record<number, ApolloClient<NormalizedCacheObj
     cache: new InMemoryCache(),
     uri: CHAIN_SUBGRAPH_URL[ChainId.AVALANCHE],
   }),
-  [ChainId.REDSTONE_GARNET]: new ApolloClient({
-    cache: new InMemoryCache(),
-    uri: CHAIN_SUBGRAPH_URL[ChainId.REDSTONE_GARNET],
-  }),
-  [ChainId.REDSTONE]: new ApolloClient({
-    cache: new InMemoryCache(),
-    uri: CHAIN_SUBGRAPH_URL[ChainId.REDSTONE],
-  }),
+  // [ChainId.REDSTONE_GARNET]: new ApolloClient({
+  //   cache: new InMemoryCache(),
+  //   uri: CHAIN_SUBGRAPH_URL[ChainId.REDSTONE_GARNET],
+  // }),
+  // [ChainId.REDSTONE]: new ApolloClient({
+  //   cache: new InMemoryCache(),
+  //   uri: CHAIN_SUBGRAPH_URL[ChainId.REDSTONE],
+  // }),
 }
